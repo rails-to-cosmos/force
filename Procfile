7e5611c399de235585f2b1765e721775d275ca7a
Rails to Cosmos <akatovda@yandex.com>
2016-01-08 23:21:19 +0300

    django skeleton

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn force.wsgi --log-file -
--- user/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_security.forms import ConfirmRegisterForm
-from wtforms import StringField
-from wtforms.validators import Required
-
-class ExtendedRegisterForm(ConfirmRegisterForm):
-    username = StringField('Username')
