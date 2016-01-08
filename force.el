c708abad550d6776407ed45ec326c92922f92aeb
Rails to Cosmos <akatovda@yandex.com>
2016-01-08 23:33:48 +0300

    emacs workflow

--- force.el
+++ force.el
@@ -1,7 +1,14 @@
-(setq force-dir "/Users/akatovda/Documents/Stuff/Force")
-
-(dz-defservice force "python"
-               :args ("manage.py" "server")
-               :cd force-dir)
+(defun force ()
+  "initialize force development workflow"
+  (interactive)
+  (defvar force-server t)
+  (dz-defservice force-server "python"
+                 :args ("app.py")
+                 :cd "/Volumes/Main/Users/akatovda/Documents/Stuff/force")
+  (elscreen-create)
+  (force-server-start)
+  (delete-other-windows)
+  (spawn-shell "*force-shell*" "/Volumes/Main/Users/akatovda/Documents/Stuff/force/")
+  (delete-other-windows))
 
 (provide 'force)
