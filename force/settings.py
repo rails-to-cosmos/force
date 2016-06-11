4cfb8e260029fb9f10433aa408a093a1438d0f72
Rails to Cosmos <akatovda@yandex.com>
2016-06-12 00:07:59 +0300

    Unicode JSON = False.

--- force/settings.py
+++ force/settings.py
@@ -61,7 +61,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    )
+    ),
+    'UNICODE_JSON': False
 }
 
 # LOGIN_REDIRECT_URL = '/'
