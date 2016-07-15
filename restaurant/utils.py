23078674a5ff5595b1e89736fa80c03434c176a1
Rails to Cosmos <akatovda@yandex.com>
2016-07-16 01:43:07 +0300

    Minor cleanups.

--- restaurant/utils.py
+++ restaurant/utils.py
@@ -3,7 +3,7 @@ from models import Menu
 
 
 def user_wants_actual_menu(request):
-    return request.GET.get('menu', '') == 'actual'
+    return request.GET.get('menu') == 'actual'
 
 
 def user_filter_by_category(request):
@@ -11,7 +11,7 @@ def user_filter_by_category(request):
 
 
 def get_actual_menu():
-    return Menu.objects.filter(date__gte=timezone.now()).order_by('date')[0]
+    return Menu.objects.filter(date__gte=timezone.now()).order_by('date').first()
 
 
 def extend_response(response, key, addition, defkey, extended=False):
